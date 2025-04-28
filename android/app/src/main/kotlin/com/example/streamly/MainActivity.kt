273041d6ddb972c09a086c6fbce4571ac11bbef8
tushar-gour <tushargour004@gmail.com>
2025-04-28 16:37:07 +0530

    app creation

--- android/app/src/main/kotlin/com/example/streamly/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.streamly
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/tushar/streamly/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tushar.streamly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
